ba4cb7520aa3b89b96268b34716def2ce0376341
Jet <rondellivey03@gmail.com>
2021-06-18 21:18:23 -0400

    Updated Scripts

--- Index.js
+++ Index.js
@@ -6,15 +6,18 @@ function RefreshData() {
         return response.json();
     })
     .then(function (obj) {
-       
+        
         document
         .getElementById("nameParent")
         .innerHTML = ""
         
         const html = obj.members.map(users => {
-            return `<li><img src="${users.avatar_url}"> ${users.username} <i class="fas fa-circle ${users.status}"></i></li>`
+            return `<li><img src="${users.avatar_url}"> ${users.username} <i class="fas fa-circle ${users.status}"></i> - <i class="fas fa-gamepad"></i> Playing ${users.game?.name}</li>`
         })
         .join('')
+        .replaceAll('- <i class="fas fa-gamepad"></i> Playing undefined', '')
+        .replace('Sir. Sebastian', `Sir. Sebastian <span class="badge bg-primary">Bot</span>`)
+        .replace('SeC Jetaime Alexandria', `SeC Jetaime Alexandria <span class="badge bg-primary">Widget Creator</span>`)
 
         document
         .querySelector("#nameParent")
